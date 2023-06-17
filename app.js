bd2c323e671324d13ae5ba7ead9c3cf7eb3be3bf
andrespalencia10 <palenciaandres503@gmail.com>
2023-06-16 21:55:55 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors=require('cors')
+
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors({
+    origin: '*'
+}))
+
+
+
+const proyectos = require('./routes/proyecto')
+
+
+
+app.use('/api/proyecto', proyectos)
+
+
+module.exports = app
+
+
